bfb73011e984940444b40b6e8a4f9466625fa1f2
Bijeesh Balakrishnan <bijbal@gmail.com>
2017-03-27 08:19:48 +0530

    rolled back test

--- School/SchoolService/SchoolModel/School.cs
+++ School/SchoolService/SchoolModel/School.cs
@@ -10,8 +10,7 @@ namespace SchoolModel
     {
         public int SchoolId { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; 
-}
+        public string Address { get; set; }
     }
 
     public abstract class SchoolPerson
