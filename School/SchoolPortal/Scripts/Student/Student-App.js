331e9b8e4dfe201595d70aea7b876e2ac085c083
Bijeesh Balakrishnan <bijbal@gmail.com>
2017-03-29 01:00:32 +0530

    new

--- School/SchoolPortal/Scripts/Student/Student-App.js
+++ School/SchoolPortal/Scripts/Student/Student-App.js
@@ -4,11 +4,19 @@
 var controller = studentApp.controller('studentController', function ($scope, studentService) {
 
     $scope.detentions = {};
+    $scope.message = "";
     var sId = document.getElementById("studentId").value;
     var date = new Date().getDate();
     $scope.calculate = function () {
         studentService.calculate(sId, date, function (data) {
+            var total = 0;
+            if ($scope.detentions != null) {
+                for (var i = 0; i < $scope.detentions.length; i++) {
+                    total += $scope.detentions[i].DetentionDuration;
+                }
+            }
 
+            $scope.message = "Total Detentions -" + total;
         });
     };
     
